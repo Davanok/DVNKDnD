304766b4c92e98e8a33057aa6e5b52c14e44e9eb
davanok <r9195253857@gmail.com>
2025-08-13 14:13:38 +0300

    separate viewModels di module

---
+++ composeApp/src/commonMain/kotlin/com/davanok/dvnkdnd/data/di/viewModelModules/newCharacterViewModelsModule.kt
@@ -0,0 +1,31 @@
+package com.davanok.dvnkdnd.data.di.viewModelModules
+
+import com.davanok.dvnkdnd.ui.pages.newEntity.newCharacter.NewCharacterViewModel
+import com.davanok.dvnkdnd.ui.pages.newEntity.newCharacter.loadingScreen.LoadingDataViewModel
+import com.davanok.dvnkdnd.ui.pages.newEntity.newCharacter.newCharacterMain.NewCharacterMainViewModel
+import com.davanok.dvnkdnd.ui.pages.newEntity.newCharacter.newCharacterSkills.NewCharacterSkillsViewModel
+import com.davanok.dvnkdnd.ui.pages.newEntity.newCharacter.newCharacterStats.NewCharacterStatsViewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+fun newCharacterViewModelsModule() = module {
+    viewModelOf(::NewCharacterViewModel)
+
+    viewModelOf(::LoadingDataViewModel)
+
+    viewModel { (vm: NewCharacterViewModel) ->
+        NewCharacterMainViewModel(
+            get(),
+            get(),
+            get(),
+            vm
+        )
+    }
+    viewModel { (vm: NewCharacterViewModel) ->
+        NewCharacterStatsViewModel(vm)
+    }
+    viewModel { (vm: NewCharacterViewModel) ->
+        NewCharacterSkillsViewModel(vm)
+    }
+}
--- composeApp/src/commonMain/kotlin/com/davanok/dvnkdnd/data/di/viewModelsModule.kt
+++ composeApp/src/commonMain/kotlin/com/davanok/dvnkdnd/data/di/viewModelsModule.kt
@@ -1,36 +1,15 @@
 package com.davanok.dvnkdnd.data.di
 
+import com.davanok.dvnkdnd.data.di.viewModelModules.newCharacterViewModelsModule
 import com.davanok.dvnkdnd.ui.pages.charactersList.CharactersListViewModel
 import com.davanok.dvnkdnd.ui.pages.dndEntityInfo.DnDEntityInfoViewModel
-import com.davanok.dvnkdnd.ui.pages.newEntity.newCharacter.NewCharacterViewModel
-import com.davanok.dvnkdnd.ui.pages.newEntity.newCharacter.loadingScreen.LoadingDataViewModel
-import com.davanok.dvnkdnd.ui.pages.newEntity.newCharacter.newCharacterMain.NewCharacterMainViewModel
-import com.davanok.dvnkdnd.ui.pages.newEntity.newCharacter.newCharacterSkills.NewCharacterSkillsViewModel
-import com.davanok.dvnkdnd.ui.pages.newEntity.newCharacter.newCharacterStats.NewCharacterStatsViewModel
-import org.koin.core.module.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 fun viewModelsModule() = module {
+    includes(newCharacterViewModelsModule())
 
     viewModelOf(::CharactersListViewModel)
-
-    viewModelOf(::LoadingDataViewModel)
-    viewModelOf(::NewCharacterViewModel)
-    viewModel { (vm: NewCharacterViewModel) ->
-        NewCharacterMainViewModel(
-            get(),
-            get(),
-            get(),
-            vm
-        )
-    }
-    viewModel { (vm: NewCharacterViewModel) ->
-        NewCharacterStatsViewModel(vm)
-    }
-    viewModel { (vm: NewCharacterViewModel) ->
-        NewCharacterSkillsViewModel(vm)
-    }
 
     viewModelOf(::DnDEntityInfoViewModel)
 }
