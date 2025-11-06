f591a1030a55a4dbd075e9d6e67dd7a6ab88dc14
davanok <r9195253857@gmail.com>
2025-11-06 16:09:36 +0300

    update libraries

--- composeApp/src/desktopMain/kotlin/com/davanok/dvnkdnd/data/platform/clipboard.kt
+++ composeApp/src/desktopMain/kotlin/com/davanok/dvnkdnd/data/platform/clipboard.kt
@@ -1,7 +1,9 @@
 package com.davanok.dvnkdnd.data.platform
 
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.ClipEntry
 import java.awt.datatransfer.StringSelection
 
+@OptIn(ExperimentalComposeUiApi::class)
 actual fun clipEntryOf(value: String): ClipEntry =
     ClipEntry(StringSelection(value))
