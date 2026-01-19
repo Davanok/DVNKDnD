9f42d032c5ecf1df098550028d4158b9bc83e403
davanok <r9195253857@gmail.com>
2026-01-19 12:28:22 +0300

    add entity dialog ui improvements

--- composeApp/src/commonMain/kotlin/com/davanok/dvnkdnd/ui/pages/characterFull/dialogs/CharacterAddEntityDialog.kt
+++ composeApp/src/commonMain/kotlin/com/davanok/dvnkdnd/ui/pages/characterFull/dialogs/CharacterAddEntityDialog.kt
@@ -4,7 +4,6 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,17 +12,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.material3.rememberSearchBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,8 +38,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.davanok.dvnkdnd.domain.entities.dndEntities.DnDEntityMin
 import com.davanok.dvnkdnd.domain.entities.dndEntities.DnDEntityWithSubEntities
@@ -40,48 +50,97 @@ import com.davanok.dvnkdnd.domain.enums.dndEnums.DnDEntityTypes
 import com.davanok.dvnkdnd.ui.components.BaseEntityImage
 import com.davanok.dvnkdnd.ui.pages.characterFull.CharacterAddEntityViewModel
 import dvnkdnd.composeapp.generated.resources.Res
+import dvnkdnd.composeapp.generated.resources.error
 import dvnkdnd.composeapp.generated.resources.hide_description
+import dvnkdnd.composeapp.generated.resources.loading
+import dvnkdnd.composeapp.generated.resources.refresh
 import dvnkdnd.composeapp.generated.resources.search_entity_type
 import dvnkdnd.composeapp.generated.resources.show_description
+import io.github.aakira.napier.Napier
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.parameter.parametersOf
 import kotlin.uuid.Uuid
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CharacterAddEntityDialogContent(
     entityType: DnDEntityTypes,
     onSelectEntityClick: (DnDEntityMin) -> Unit,
     onEntityInfoClick: (DnDEntityMin) -> Unit,
-    viewModel: CharacterAddEntityViewModel = koinViewModel { parametersOf(entityType) }
+    viewModel: CharacterAddEntityViewModel = koinViewModel(key = entityType.name) { parametersOf(entityType) }
 ) {
     val entities = viewModel.entitiesFlow.collectAsLazyPagingItems()
     val query by viewModel.searchQuery.collectAsStateWithLifecycle()
 
     var expandedCard: Uuid? by remember { mutableStateOf(null) }
+    var searchBarExpanded by remember { mutableStateOf(false) }
 
-    Column {
-        Spacer(modifier = Modifier.height(16.dp))
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        SearchBar(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            inputField = {
+                SearchBarDefaults.InputField(
+                    query = query,
+                    onQueryChange = viewModel::setSearchQuery,
+                    onSearch = {  },
+                    expanded = searchBarExpanded,
+                    onExpandedChange = { searchBarExpanded = it },
+                    placeholder = {
+                        Text(
+                            text = stringResource(
+                                Res.string.search_entity_type,
+                                stringResource(entityType.stringRes)
+                            )
+                        )
+                    }
+                )
+            },
+            state = rememberSearchBarState()
+        )
+        Spacer(Modifier.height(8.dp))
+        EntitiesColumn(
+            entities = entities,
+            expandedCard = expandedCard,
+            onExpandClick = { entityId ->
+                expandedCard = entityId.takeIf { it != expandedCard }
+            },
+            onEntityInfoClick = onEntityInfoClick,
+            onSelectEntityClick = onSelectEntityClick
+        )
+    }
+}
 
-        OutlinedTextField(
-            value = query,
-            onValueChange = viewModel::setSearchQuery,
-            modifier = Modifier.fillMaxWidth(),
-            singleLine = true,
-            label = {
-                Text(
-                    text = stringResource(
-                        Res.string.search_entity_type,
-                        stringResource(entityType.stringRes))
+@Composable
+private fun EntitiesColumn(
+    entities: LazyPagingItems<DnDEntityWithSubEntities>,
+    expandedCard: Uuid?,
+    onExpandClick: (entityId: Uuid) -> Unit,
+    onEntityInfoClick: (DnDEntityMin) -> Unit,
+    onSelectEntityClick: (DnDEntityMin) -> Unit,
+) {
+    LaunchedEffect(entities.loadState) {
+        Napier.d { entities.loadState.toString() }
+    }
+
+    val prependState = entities.loadState.prepend
+    val refreshState = entities.loadState.refresh
+    val appendState = entities.loadState.append
+
+    LazyColumn {
+        if (prependState is LoadState.Loading)
+            item { LoadingRow() }
+
+        if (refreshState is LoadState.Error)
+            item {
+                ErrorRow(
+                    error = refreshState.error,
+                    onRetry = { entities.retry() }
                 )
             }
-        )
-
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            contentPadding = PaddingValues(top = 16.dp)
-        ) {
-            // TODO: add placeholders
+        if (refreshState is LoadState.NotLoading)
             items(
                 count = entities.itemCount,
                 key = { entities[it]?.id ?: Uuid.random() }
@@ -90,17 +149,57 @@ fun CharacterAddEntityDialogContent(
                     EntityCard(
                         entity = entity,
                         expanded = entity.id == expandedCard,
-                        onExpandClick = {
-                            expandedCard =
-                                if (expandedCard == entity.id) null
-                                else entity.id
-                        },
+                        onExpandClick = { onExpandClick(entity.id) },
                         onInfoClick = onEntityInfoClick,
                         onAddClick = onSelectEntityClick,
                         modifier = Modifier.fillMaxWidth()
                     )
                 }
             }
+
+        if (appendState is LoadState.Loading)
+            item { LoadingRow() }
+
+        if (appendState is LoadState.Error)
+            item {
+                ErrorRow(
+                    error = appendState.error,
+                    onRetry = { entities.retry() }
+                )
+            }
+    }
+}
+
+@Composable
+private fun LoadingRow() {
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp, vertical = 16.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        CircularProgressIndicator()
+        Spacer(Modifier.width(12.dp))
+        Text(text = stringResource(Res.string.loading))
+    }
+}
+@Composable
+private fun ErrorRow(
+    error: Throwable,
+    onRetry: () -> Unit
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp, vertical = 16.dp),
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = error.message ?: stringResource(Res.string.error),
+            textAlign = TextAlign.Center
+        )
+
+        TextButton(
+            modifier = Modifier.align(Alignment.End),
+            onClick = onRetry
+        ) {
+            Text(text = stringResource(Res.string.refresh))
         }
     }
 }
@@ -117,16 +216,13 @@ private fun EntityCard(
     modifier: Modifier = Modifier
 ) {
     val entityMin = entity.toDnDEntityMin()
-    OutlinedCard(
-        modifier = modifier,
-        onClick = { onAddClick(entityMin) }
+    Column(
+        modifier = modifier.clickable { onAddClick(entityMin) }
     ) {
         Column(
-            modifier = Modifier.padding(12.dp),
+            modifier = Modifier.padding(vertical = 8.dp, horizontal = 12.dp)
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
+            Row {
                 BaseEntityImage(
                     entity = entityMin,
                     onClick = { onInfoClick(entityMin) },
@@ -134,7 +230,7 @@ private fun EntityCard(
                         .size(56.dp)
                 )
 
-                Spacer(modifier = Modifier.width(12.dp))
+                Spacer(Modifier.width(8.dp))
 
                 Column(
                     modifier = Modifier.weight(1f)
@@ -150,40 +246,51 @@ private fun EntityCard(
                 }
 
                 IconButton(
-                    onClick = onExpandClick,
-                    modifier = Modifier.align(Alignment.Bottom)
+                    onClick = onExpandClick
                 ) {
                     Icon(
                         imageVector = Icons.Default.ArrowDropDown,
                         contentDescription =
                             if (expanded) stringResource(Res.string.hide_description)
                             else stringResource(Res.string.show_description),
-                        modifier = Modifier.rotate(if (expanded) 180f else 0f))
+                        modifier = Modifier.rotate(if (expanded) 180f else 0f)
+                    )
                 }
             }
             AnimatedVisibility(visible = expanded) {
                 Column {
-                    Spacer(Modifier.height(12.dp))
-                    Text(text = entity.description)
+                    Spacer(Modifier.height(8.dp))
+                    Text(
+                        text = entity.description,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
                 }
             }
+            if (entity.subEntities.isNotEmpty()) {
+                Spacer(Modifier.height(8.dp))
+                LazyRow {
+                    items(
+                        items = entity.subEntities,
+                        key = { it.id }
+                    ) { subEntity ->
+                        Row(
+                            modifier = Modifier.clickable { onAddClick(subEntity) }
+                        ) {
+                            BaseEntityImage(
+                                entity = subEntity,
+                                modifier = Modifier.size(24.dp)
+                            )
 
-            entity.subEntities.forEach { subEntity ->
-                HorizontalDivider(Modifier.padding(vertical = 8.dp))
+                            Spacer(modifier = Modifier.width(8.dp))
 
-                Row(
-                    modifier = Modifier.clickable { onAddClick(subEntity) }
-                ) {
-                    BaseEntityImage(
-                        entity = subEntity,
-                        modifier = Modifier.size(24.dp)
-                    )
+                            Text(text = subEntity.name)
 
-                    Spacer(modifier = Modifier.width(8.dp))
-
-                    Text(text = subEntity.name)
+                            VerticalDivider()
+                        }
+                    }
                 }
             }
         }
+        HorizontalDivider()
     }
 }
