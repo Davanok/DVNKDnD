a3bd665343c365b5e5e44a4fe3e5b09e30bb4894
davanok <r9195253857@gmail.com>
2026-02-08 16:45:03 +0300

    add icons for acid, force and necrotic damage types

--- composeApp/src/commonMain/kotlin/com/davanok/dvnkdnd/domain/enums/dndEnums/DamageTypes.kt
+++ composeApp/src/commonMain/kotlin/com/davanok/dvnkdnd/domain/enums/dndEnums/DamageTypes.kt
@@ -20,13 +20,13 @@ import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.StringResource
 
 enum class DamageTypes(val stringRes: StringResource, val drawableRes: DrawableResource?, val color: Color? = null) { // TODO: drawable nullable is temporary
-    ACID        (Res.string.damage_type_acid,           null,                               Color(0xFF80B000)),
+    ACID        (Res.string.damage_type_acid,           Res.drawable.damage_type_acid,      Color(0xFF80B000)),
     BLUDGEONING (Res.string.damage_type_bludgeoning,    null,                               null),
     COLD        (Res.string.damage_type_cold,           Res.drawable.damage_type_cold,      Color(0xFF3399CC)),
     FIRE        (Res.string.damage_type_fire,           Res.drawable.damage_type_fire,      Color(0xFFEE5500)),
-    FORCE       (Res.string.damage_type_force,          null,                               Color(0xFFCC3333)),
+    FORCE       (Res.string.damage_type_force,          Res.drawable.damage_type_force,     Color(0xFFCC3333)),
     LIGHTNING   (Res.string.damage_type_lightning,      Res.drawable.damage_type_lightning, Color(0xFF3366CC)),
-    NECROTIC    (Res.string.damage_type_necrotic,       null,                               Color(0xFF40b050)),
+    NECROTIC    (Res.string.damage_type_necrotic,       Res.drawable.damage_type_necrotic,  Color(0xFF40b050)),
     PIERCING    (Res.string.damage_type_piercing,       null,                               null),
     POISON      (Res.string.damage_type_poison,         Res.drawable.damage_type_poison,    Color(0xFF44BB00)),
     PSYCHIC     (Res.string.damage_type_psychic,        Res.drawable.damage_type_psychic,   Color(0xFFCC77AA)),
